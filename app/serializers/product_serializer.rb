8b323b04d555f99da4078f21655fc35b1356f46e
Scott Atkins <satkins16@gmail.com>
2018-01-10 01:40:33 +0000

    automatically backed up by learn

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -6,6 +6,6 @@ class ProductSerializer
     serialized_product += '"name": "' + product.name + '", '
     serialized_product += '"price": ' + product.price.to_s + ', '
     serialized_product += '"inventory": ' + product.inventory.to_s + ', '
-    serialized_product += '"description": ' + product.id.to_s +
+    serialized_product += '"description": "' + product.description + '"'
 
 end
