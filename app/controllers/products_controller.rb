ce2f852d21cb2c2f1cf816ae6bea1064aad10d49
Scott Atkins <satkins16@gmail.com>
2018-01-10 01:42:21 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,6 +24,7 @@ class ProductsController < ApplicationController
 
   def product_data
     product = Product.find(params[:id])
+    render json: ProductSerializer.serialize(product)
 
   private
 
